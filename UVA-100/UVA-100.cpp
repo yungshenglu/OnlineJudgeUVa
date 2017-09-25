e8d2b4da389a85fff4fe0bfd1568705acaeab121
yungshenglu <yungshenglu1994@gmail.com>
2017-09-25 18:32:46 +0800

    UVA-100.cpp accepted

---
+++ UVA-100/UVA-100.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+
+using namespace std;
+
+/* Calculate the cycle length */
+int cycleLength(int n) {
+    if (n != 1) {
+        return 1 + ((n % 2) ? cycleLength(3 * n + 1) : cycleLength(n / 2));
+    } else {
+        return 1;
+    }
+}
+
+int main(void) {
+    int i, j, min_i_j, max_i_j;
+    int max_length, term_length;
+
+    while (scanf("%d %d", &i, &j) != EOF) {
+        max_length = 0;
+
+        // Get the greatest and lowest value in i and j
+        min_i_j = (i < j) ? i : j;
+        max_i_j = (i > j) ? i : j;
+
+        while (min_i_j <= max_i_j) {
+            term_length = cycleLength(min_i_j);
+            max_length = (term_length > max_length) ? term_length : max_length;
+            ++min_i_j;  
+        }
+
+        // Output results
+        printf("%d %d %d\n", i, j, max_length);
+    }
+
+    return 0;
+}
