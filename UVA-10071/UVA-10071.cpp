bef3bbaff86e74ac328009ae190bd7fe1bffd97f
yungshenglu <yungshenglu1994@gmail.com>
2018-04-05 16:48:37 +0800

    UVA-10071 accepted

--- UVA-10071/UVA-10071.cpp
+++ UVA-10071/UVA-10071.cpp
@@ -7,7 +7,7 @@ int main(void) {
     int v, t;
 
     while (scanf("%d %d", &v, &t) != EOF) {
-        // Calculate the displacement be in twice of that time and print it
+        // Calculate the displacement be in twice of that time and print the result
         printf("%d\n", 2 * v * t);
     }
 
