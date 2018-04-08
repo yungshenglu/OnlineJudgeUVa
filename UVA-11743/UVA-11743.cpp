74c4e3838ccb2383bb4b15c57d15e44b518b613b
yungshenglu <yungshenglu1994@gmail.com>
2018-04-08 20:48:30 +0800

    UVA-11743 accepted

--- UVA-11743/UVA-11743.cpp
+++ UVA-11743/UVA-11743.cpp
@@ -6,18 +6,20 @@ using namespace std;
 
 int main(void) {
     int T;
-    string credit;
+    char credit[4];
 
     while (scanf("%d", &T) != EOF) {
         for (int t = 0; t < T; ++t) {      
             int odd = 0, even = 0;
 
+            // For input each 4 digit number and add into odd and even digit checksum respectively
             for (int i = 0; i < 4; ++i) {
-                cin >> credit;
+                scanf("%s", credit);
                 odd += ((credit[1] - '0') + (credit[3] - '0'));
                 even += (((credit[0] - '0') * 2) / 10 + ((credit[0] - '0') * 2) % 10 + ((credit[2] - '0') * 2) / 10 + ((credit[2] - '0') * 2) % 10);
             }
 
+            // Print the result
             if ((odd + even) % 10)
                 printf("Invalid\n");
             else
