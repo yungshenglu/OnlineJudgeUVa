a31148a333c277aadffaf3eb36a240e1983e36b2
yungshenglu <yungshenglu1994@gmail.com>
2018-05-03 14:44:26 +0800

    UVA-10721 accepted

--- UVA-10721/UVA-10721.cpp
+++ UVA-10721/UVA-10721.cpp
@@ -12,6 +12,7 @@ int main(void) {
         // Initialize DP table
         long long int BC[MAX][MAX] = { 0 };
 
+        // Use DP to find out all possible combinations
         BC[0][0] = 1;
         for (int i = 1; i <= n; ++i) {
             for (int j = 1; j <= k; ++j) {
